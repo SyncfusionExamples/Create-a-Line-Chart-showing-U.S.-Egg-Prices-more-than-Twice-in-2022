9947dd3a31caf0c60e50e73aaab9b7d80e8f45ce
Harsha <harshamidadhala.k@syncfusion.com>
2025-04-29 11:23:09 +0530

    version updated

---
+++ EggPriceChart/EggPriceChart/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EggPriceChart
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- EggPriceChart/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace EggPriceChart;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
